ebe55f54697b2f00fc554a4e864803272911faaa
KurakuJaeger <apecschoolclark@gmail.com>
2026-07-12 09:47:14 +0800

    Support gitignored local mail configuration

--- my-php-app/src/config/mail.php
+++ my-php-app/src/config/mail.php
@@ -1,6 +1,14 @@
 <?php
 function mailConfig(): array
 {
+    $localConfigFile = __DIR__ . '/mail.local.php';
+    if (is_file($localConfigFile)) {
+        $localConfig = require $localConfigFile;
+        if (is_array($localConfig)) {
+            return $localConfig;
+        }
+    }
+
     return [
         'host' => getenv('MAIL_HOST') ?: '', 'port' => (int) (getenv('MAIL_PORT') ?: 587),
         'username' => getenv('MAIL_USERNAME') ?: '', 'password' => getenv('MAIL_PASSWORD') ?: '',
